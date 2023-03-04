6f680b4b91de242b319b4cbf2aae3abca543311a
maciej-kor <korczakmaciek@gmail.com>
2023-03-05 00:54:33 +0100

    first commit

---
+++ src/main/java/com/projects/igritdatacollector/IgritDataCollectorApplication.java
@@ -0,0 +1,13 @@
+package com.projects.igritdatacollector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IgritDataCollectorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IgritDataCollectorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/projects/igritdatacollector/IgritDataCollectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.igritdatacollector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IgritDataCollectorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
