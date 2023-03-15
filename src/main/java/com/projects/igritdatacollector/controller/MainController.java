793ab8c2fa871de1a43c076d457c4873ea2a04b5
maciej-kor <korczakmaciek@gmail.com>
2023-03-15 21:40:44 +0100

    added get method to controller class

--- src/main/java/com/projects/igritdatacollector/controller/MainController.java
+++ src/main/java/com/projects/igritdatacollector/controller/MainController.java
@@ -1,11 +1,11 @@
 package com.projects.igritdatacollector.controller;
 
 import com.projects.igritdatacollector.model.AnnouncementDetailsModel;
-import com.projects.igritdatacollector.repository.AnnouncementDetailsRepository;
 import com.projects.igritdatacollector.service.AnnouncementDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +21,16 @@ public class MainController {
     @GetMapping("/")
     public List<AnnouncementDetailsModel> getAllAnnouncement() {
         return announcementDetailsService.getAllAnnouncementDetailsModel();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<AnnouncementDetailsModel> getAllAnnouncement(@PathVariable Long id) {
+        AnnouncementDetailsModel announcementDetailsModel = announcementDetailsService.getAnnouncementDetailsModel(id);
+        if (announcementDetailsModel == null){
+            return ResponseEntity.notFound().build();
+        } else {
+            return ResponseEntity.ok(announcementDetailsModel);
+        }
     }
 
 }
